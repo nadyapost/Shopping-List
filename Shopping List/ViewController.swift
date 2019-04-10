71db53bd9b70b0acea0087e3443651ff72be5649
nadyapost <nadyapostr@gmail.com>
2019-04-10 14:57:59 +1000

    Initial Commit

---
+++ Shopping List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shopping List
+//
+//  Created by Nadya Postriganova on 10/4/19.
+//  Copyright © 2019 Nadya Postriganova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
