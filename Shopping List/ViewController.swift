e7c42e4bdc5d1e71582d3c5452a87b9673fcb434
nadyapost <nadyapostr@gmail.com>
2019-04-11 20:41:32 +1000

    Add alert view controller with text field.

--- Shopping List/ViewController.swift
+++ Shopping List/ViewController.swift
@@ -8,13 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+  
+  var shoppingList: [String] = []
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewItem))
+ 
+  }
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return shoppingList.count
   }
 
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    cell.textLabel?.text = shoppingList[indexPath.row]
+    return cell
+  }
+  
+  @objc func addNewItem() {
+    let ac = UIAlertController(title: "New item", message: nil, preferredStyle: .alert)
+    ac.addTextField()
+    let submitAction = UIAlertAction(title: "Add", style: .default) { [weak self, weak ac] _ in
+      guard let item = ac?.textFields?[0].text else { return }
+      self?.addNewItemToList(item)
+    }
+    ac.addAction(submitAction)
+    present(ac, animated: true)
 
+  }
+  func addNewItemToList(_ item:  String) {
+    shoppingList.insert(item, at: 0)
+    let indexPath = IndexPath(row: 0, section: 0)
+    tableView.insertRows(at: [indexPath], with: .automatic)
+
+  }
 }
 
